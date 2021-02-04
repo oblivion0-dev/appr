ba60f43491f53823606e4f17e204182aee0a7515
Nicolas GALLOIS <ngallois@basilix>
2021-02-04 13:37:19 +0100

    f_CustomXticsDatePlot function added and scripts improved

---
+++ Lib/f_CustomXticsDatePlot.R
@@ -0,0 +1,26 @@
+# Function : Creating custom breaks and xtics labels for plots using dates
+
+# Inputs : VecX : vector of n integers ranging from 1 to n (number of days of simulation)
+#          VecDate : vector of corresponding dates in aaaa-mm-dd format
+ 
+f_CustomXticsDatePlot<-function(vecX,vecDate,startYear){
+  
+  breakpoints <- vector()
+  xLabels <- vector()
+
+  pos <- 1
+
+  while (pos < length(vecX))
+  {
+    nbDays <- 365
+    if ((startYear+1)%%4 == 0) nbDays <- 366
+    pos <- pos + nbDays
+    breakpoints <- append(breakpoints,pos)
+    xLabels <- append(xLabels,vecDate[pos])
+    startYear <- startYear + 1
+  }
+
+  returnList <- list("breaks" = breakpoints[1:length(breakpoints)-1], "labels" = xLabels[1:length(xLabels)-1])
+
+  return(returnList)
+}
